0700f06a7f8fac4a1a7a09996f7e73eeb89227b7
sineckers <yasar.ylmz.yt@gmail.com>
2022-09-28 20:57:43 +0300

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  const config = new DocumentBuilder()
+    .setTitle('Sineckers NestJS Test')
+    .setDescription('Sineckers NestJS Test REST API with MongoDB')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  userId: string;
+}
